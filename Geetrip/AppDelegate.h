1a64314a3f76da0de9c9fd6973a82850b48948ba
Zhao Chengfeng <zhaochengfengios@163.com>
2015-07-12 08:02:57 +0800

    Initial Commit

---
+++ Geetrip/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Geetrip
+//
+//  Created by chengfeng on 15/7/12.
+//  Copyright (c) 2015年 Geetrip Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Geetrip/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Geetrip
+//
+//  Created by chengfeng on 15/7/12.
+//  Copyright (c) 2015年 Geetrip Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
